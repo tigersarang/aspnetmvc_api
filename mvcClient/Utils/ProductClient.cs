ab4e320f0ba3e9be17a1e187df039240318d73c3
David choi <tigersarang@naver.com>
2023-06-20 19:53:29 +0900

    Separate the apiClient into a partial class.

---
+++ mvcClient/Utils/ProductClient.cs
@@ -0,0 +1,59 @@
+﻿using CommLibs.Dto;
+using CommLibs.Models;
+using Microsoft.AspNetCore.Mvc;
+using mvcClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace mvcClient.Utils
+{
+    public partial class ApiClient
+    {
+
+        // jwtvuecrudapp의 ProductsController의 모든 메서드 대응
+
+        //public async Task<IActionResult> GetAll([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10, [FromQuery] string search = null)
+        public async Task<HttpResponseMessage> GetAll(int pageNumber = 1, int pageSize = 10, string search = null)
+        {
+            var response = await _httpClient.GetAsync($"products?pageNumber={pageNumber}&pageSize={pageSize}&search={search}");
+            return response;
+        }
+
+        //public async Task<IActionResult> GetById(int id)
+        public async Task<Product> GetById(int id)
+        {
+            var response = await _httpClient.GetAsync($"products/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<Product>();
+            } else
+            {
+                return null;
+            }
+        }
+
+        //public async Task<IActionResult> Create([FromBody] Product product)
+        public async Task<HttpResponseMessage> Create(Product product)
+        {
+            var response = await _httpClient.PostAsJsonAsync("products", product);
+            return response;
+        }
+
+        //public async Task<IActionResult> Update(int id, [FromBody] Product product)
+        public async Task<HttpResponseMessage> UpdateAsync(int id, Product product)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"products/{id}", product);
+            return response;
+        }
+
+        //public async Task<IActionResult> Delete(int id)
+        public async Task<bool> Delete(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"products/{id}");
+            return response.IsSuccessStatusCode;
+        }
+
+    }
+}
---
+++ mvcClient/Utils/ReplyClient.cs
@@ -0,0 +1,27 @@
+﻿using CommLibs.Dto;
+using CommLibs.Models;
+using Microsoft.AspNetCore.Mvc;
+using mvcClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace mvcClient.Utils
+{
+    public partial class ApiClient
+    {
+
+
+        // Reply 작업
+        public async Task<Reply?> CreateReply(Reply reply)
+        {
+            var response = await _httpClient.PostAsJsonAsync("replies", reply);
+            return await response.Content.ReadFromJsonAsync<Reply>();
+        }
+        public async Task<bool> DeleteReply(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"replies/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
